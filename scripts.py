236a02c810f72b5069ab283b10ffa88dd12f0533
WarLikeLaux <warlikelaux080399@mail.ru>
2023-11-12 15:49:48 +0600

    Enhanced the code

--- scripts.py
+++ scripts.py
@@ -10,23 +10,24 @@ from datacenter.models import (
     Schoolkid
 )
 
+class NoLessonException(Exception):
+    pass
+
 
 def get_schoolkid(schoolkid_name):
     try:
         schoolkid = Schoolkid.objects.get(full_name__contains=schoolkid_name)
         return schoolkid
     except MultipleObjectsReturned:
-        print(
+        return (
             f"Найдено несколько учеников с именем {schoolkid_name} - "
             f"уточните запрос добавив фамилию или отчество."
         )
-        exit(1)
     except ObjectDoesNotExist:
-        print(
+        return (
             f"Ученик с именем {schoolkid_name}"
-            f"не найден - проверьте корректность ввода."
+            f" не найден - проверьте корректность ввода."
         )
-        exit(1)
 
 
 def fix_marks(schoolkid_name):
@@ -35,7 +36,7 @@ def fix_marks(schoolkid_name):
         schoolkid=schoolkid,
         points__in=[2, 3]
     )
-    return bad_marks.update(points=5) 
+    return bad_marks.update(points=5)
 
 
 def remove_chastisements(schoolkid_name):
@@ -46,22 +47,27 @@ def remove_chastisements(schoolkid_name):
 def get_lesson(schoolkid, subject_title, mode="latest"):
     order_by_map = {"latest": "-date", "first": "date", "random": "?"}
     if mode not in order_by_map:
-        print(
+        return (
             f"Неверный режим выбора урока: {mode}, "
             f"допустимые значения: latest, first, random"
         )
-        exit(1)
 
     lessons = Lesson.objects.filter(
         year_of_study=schoolkid.year_of_study,
         group_letter=schoolkid.group_letter,
         subject__title=subject_title,
     )
-    lesson = lessons.order_by(order_by_map[mode]).first()
+    commendations = Commendation.objects.filter(
+        schoolkid=schoolkid,
+        subject__title=subject_title
+    )
+    lessons_without_commendation = lessons.exclude(
+        date__in=commendations.values_list('created', flat=True)
+    )
+    lesson = lessons_without_commendation.order_by(order_by_map[mode]).first()
 
     if lesson is None:
-        print(f"Урок по предмету `{subject_title}` без похвалы не найден.")
-        exit(1)
+        raise NoLessonException(f"Урок по предмету `{subject_title}` без похвалы не найден.")
 
     return lesson
 
@@ -97,13 +103,27 @@ def get_random_commendation_text():
     return random.choice(commendations)
 
 
+def format_commendation(commendation):
+    return (
+        f"Commendation: {commendation.text}"
+        f", Schoolkid: {commendation.schoolkid.full_name}"
+        f", Date: {commendation.created}"
+        f", Subject: {commendation.subject.title}"
+        f", Teacher: {commendation.teacher.full_name}"
+    )
+
+
 def create_commendation(schoolkid_name, subject_title):
     schoolkid = get_schoolkid(schoolkid_name)
-    lesson = get_lesson(schoolkid, subject_title)
-    return Commendation.objects.create(
+    try:
+        lesson = get_lesson(schoolkid, subject_title)
+    except NoLessonException as error:
+        return error
+    created_commendation = Commendation.objects.create(
         text=get_random_commendation_text(),
         schoolkid=schoolkid,
         created=lesson.date,
         subject=lesson.subject,
         teacher=lesson.teacher,
     )
+    return format_commendation(created_commendation)
